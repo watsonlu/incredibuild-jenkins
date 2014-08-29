6df6de6065665da00b6740f4652c75471a1b3907
Luke Watson <luwatson90@gmail.com>
2014-08-29 16:47:00 -0400

    Add some info about that unused abstract

--- src/main/java/org/jenkinsci/plugins/incredibuild/Commands/AbstractCommand.java
+++ src/main/java/org/jenkinsci/plugins/incredibuild/Commands/AbstractCommand.java
@@ -2,6 +2,8 @@ package org.jenkinsci.plugins.incredibuild.Commands;
 
 /**
  * Created by lwatson on 8/28/2014.
+ * 
+ * not used at the moment but will prob be used in the future.
  */
 public abstract class AbstractCommand implements Command {
 
